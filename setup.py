a1524cca946c19a1db2686f5661d3cb818c57fe0
Xenaschke <xcroucamp@gmail.com>
2020-02-10 10:30:40 +0200

    function 5 review

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='eishkom',
+    name='Eskom_Predict',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
