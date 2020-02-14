b572db01e0344ec6d299f49da9374f7edc8449df
MilkShaikh3 <MilkShaikh3@users.noreply.github.com>
2020-02-14 09:45:00 +0200

    Update Func4 docstrings

--- Eskom_Predict/Function4.py
+++ Eskom_Predict/Function4.py
@@ -9,7 +9,17 @@ Args:
     df (dataframe): pandas dataframe with tweets
 
 Returns:
-    dataframe: returns a modified dataframe that includes two new columns that contain information about the municipality and hashtag of the tweet.
+    dataframe: returns a modified dataframe that includes two new columns that contain information about the municipality and hashtag of the tweet (in lowercase).
+
+Examples:
+    >>> extract_municipality_hashtags(twitter_df.copy())
+
+    Tweets	                                                        Date	                municipality	hashtags
+0	@BongaDlulane       Please send an email to mediades...	        2019-11-29 12:50:54	    NaN	            NaN
+1	@saucy_mamiie Pls log a call on 0860037566	                    2019-11-29 12:46:53	    NaN	            NaN
+2	@BongaDlulane Query escalated to media desk.	                2019-11-29 12:46:10	    NaN	            NaN
+3	Before leaving the office this afternoon, head...	            2019-11-29 12:33:36	    NaN	            NaN
+4	#ESKOMFREESTATE #MEDIASTATEMENT : ESKOM SUSPEN...	            2019-11-29 12:17:43	    NaN	            [#eskomfreestate, #mediastatement]
 
 """
 
